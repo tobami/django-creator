ef7705fab7046c1e0d2fab314610455d0d0d3ae9
Miquel Torres <tobami@gmail.com>
2017-11-07 20:38:14 +0000

    Add Procfile and heroku settings and requirements. - Configure WSGI application - Configure SSL - Add Whitenoise and Gunicorn to requirements

---
+++ Procfile
@@ -0,0 +1,2 @@
+release: ./manage.py migrate --noinput
+web: gunicorn newproject.wsgi --log-file -
---
+++ newproject/settings/heroku.py
@@ -0,0 +1,8 @@
+from .prod import *  # noqa
+
+WSGI_APPLICATION = 'newproject.wsgi.application'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')

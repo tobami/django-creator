dfbcc7440ce66c832bf7e01deb5923f3a4cf0b0f
Miquel Torres <tobami@gmail.com>
2017-11-07 22:07:26 +0000

    Add herokuapps to allowed hosts

--- newproject/settings/heroku.py
+++ newproject/settings/heroku.py
@@ -6,3 +6,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['.herokuapp.com']

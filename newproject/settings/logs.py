0e8fefbc56e7ed422f4feb16cccb6c93e42d1e70
Miquel Torres <tobami@gmail.com>
2017-12-25 15:47:10 +0000

    More complete log configuration

--- newproject/settings/logs.py
+++ newproject/settings/logs.py
@@ -2,6 +2,13 @@ import os
 
 from .django import INSTALLED_APPS
 
+LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
+LOG_HANDLERS = ['console']
+
+RAVEN_CONFIG = {'dsn': os.environ.get('RAVEN_DSN')}
+if RAVEN_CONFIG['dsn']:
+    LOG_HANDLERS.append('sentry')
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -10,30 +17,42 @@ LOGGING = {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
         }
-    }
-}
-
-LOGGING['loggers'] = {
-    'django': {
-        'handlers': ['console'],
-        'level': 'INFO',
-        'propagate': False,
     },
-    'newproject': {
-        'handlers': ['console'],
-        'level': 'INFO',
-        'propagate': False,
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'newproject': {
+            'handlers': LOG_HANDLERS,
+            'level': LOG_LEVEL,
+            'propagate': False,
+        }
     }
 }
 
 # Sentry
-RAVEN_CONFIG = {'dsn': os.environ.get('RAVEN_DSN')}
 
 if RAVEN_CONFIG['dsn']:
     INSTALLED_APPS += [
         'raven.contrib.django.raven_compat',
     ]
-    LOGGING['handlers']['sentry'] = {
+    LOGGING['root'] = {
         'level': 'WARNING',
+        'handlers': ['sentry'],
+    }
+    LOGGING['handlers']['sentry'] = {
+        'level': 'ERROR',
         'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
+    }
+    LOGGING['loggers']['raven'] = {
+        'level': 'DEBUG',
+        'handlers': ['console'],
+        'propagate': False,
+    }
+    LOGGING['loggers']['sentry.errors'] = {
+        'level': 'DEBUG',
+        'handlers': ['console'],
+        'propagate': False,
     }
